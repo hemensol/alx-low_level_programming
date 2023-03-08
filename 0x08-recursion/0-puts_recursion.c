693854dc2e3bd9d0b826401bc0ef46582f67a782
hemensol <hemensol14@yahoo.com>
2023-03-08 18:32:11 +0300

    linting fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 void _puts_recursion(char *s)
+/**This function prints a string*/
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+if (*s)
+{
+    _putchar(*s);
+    _puts_recursion(s + 1);
+}
+else
+{
+    _putchar('\n');
+}
 }
