18f618df3a8f2789b9131f978f6395ec1f55ab67
hemensol <hemensol14@yahoo.com>
2023-03-08 18:43:38 +0300

    lint 100

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts_recursion - prints a string
- * @s input
+ * @s: input
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-    _putchar(*s);
-    _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
 }
